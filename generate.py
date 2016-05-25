2373f60be84d1f171844d2e6854390821dc2f4f5
c3o <github@c3o.org>
2016-05-25 15:51:48 +0200

    Use subdirectories for languages

--- generate.py
+++ generate.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2.7
-import json
+import json, os
 from jinja2 import Template
 
 for lang in ['en']:
-    with open('%s.html' % lang, 'w') as out:
+    if not os.path.exists(lang):
+        os.makedirs(lang)
+    with open('%s/index.html' % lang, 'w') as out:
         with open('template.html') as t:
             with open('%s.json' % lang) as f:
                 out.write(Template(t.read()).render(**json.load(f)))
