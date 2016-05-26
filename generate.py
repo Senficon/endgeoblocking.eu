98db5f9cc053f46b19f2aaf9bf6804e4b15715c9
c3o <github@c3o.org>
2016-05-26 22:02:07 +0200

    Improve variable naming

--- generate.py
+++ generate.py
@@ -3,10 +3,10 @@ import json, os
 from jinja2 import Template
 
 for lang in ['en',  'de']:
-    urllang = '.' if (lang=='en') else lang
-    if not os.path.exists(urllang):
-        os.makedirs(urllang)
-    with open('%s/index.html' % urllang, 'w') as out:
+    directory = '.' if (lang=='en') else lang
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open('%s/index.html' % directory, 'w') as out:
         with open('template.html') as t:
             with open('%s.json' % lang) as f:
                 output = Template(t.read()).render(**json.load(f, encoding='utf-8'))
