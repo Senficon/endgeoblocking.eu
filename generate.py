5093fe8e09387050eed02a539b9b4659a0ca8331
c3o <github@c3o.org>
2016-05-27 14:49:52 +0200

    Add ES language

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@
 import json, os
 from jinja2 import Template
 
-for lang in ['en',  'de']:
+for lang in ['en',  'de', 'es']:
     directory = '.' if (lang=='en') else lang
     if not os.path.exists(directory):
         os.makedirs(directory)
