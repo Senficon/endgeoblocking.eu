6e308192b6e8a321867a1aa34113563a399ab1bd
c3o <github@c3o.org>
2016-05-30 12:16:14 +0200

    Add EL

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@
 import json, os
 from jinja2 import Template
 
-for lang in ['en',  'de', 'es']:
+for lang in ['en', 'de', 'es', 'el']:
     directory = '.' if (lang=='en') else lang
     if not os.path.exists(directory):
         os.makedirs(directory)
